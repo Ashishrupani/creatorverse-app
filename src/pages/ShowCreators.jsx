9b1efe1797410e0367f4f53c074203a042f90c25
Ashishrupani <143018180+Ashishrupani@users.noreply.github.com>
2025-08-17 20:10:49 -0500

    editing done for show creator

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,20 +1,33 @@
 import "../styles/ShowCreators.css";
 import "@picocss/pico";
 
-const ShowCreators = () => {
+const ShowCreators = ({name, imageUrl, description, Url}) => {
   return (
     <div className="wrapper">
-        <div className="container-custom">
-          <div className="left">this is left</div>
-          <div className="right">this is right</div>
+      <div className="container-custom">
+        <div className="left">
+          <img
+            src={imageUrl}
+            alt="content creator's picture"
+          />
         </div>
+        <div className="right">
+          <article>
+            <header>{name}</header>
+            <textarea name="read-only" readonly = 'true'>
+              {description}
+            </textarea>
+            <footer>{Url}</footer>
+          </article>
+        </div>
+      </div>
 
-        <div className="article">
-          <div class="grid">
-            <button class="secondary">Secondary</button>
-            <button class="contrast">Contrast</button>
-          </div>
+      <div className="article">
+        <div class="grid">
+          <button class="secondary">Edit</button>
+          <button class="primary">Done</button>
         </div>
+      </div>
     </div>
   );
 };
